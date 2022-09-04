e1be0d894fbb20fc85531bf48c7137fc2b18c8ba
unknown <william.bele@edhec.com>
2022-09-04 11:02:44 +0200

    adding advance time and native mint

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use cosmwasm_std::Coin;
+use cw_multi_test::{App, BankSudo, SudoMsg};
+
+pub trait TimeTravel {
+    fn advance_time(&mut self, nb_blocks_to_advance: u64, nb_seconds_to_advance: u64);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl TimeTravel for App {
+    fn advance_time(&mut self, nb_blocks_to_advance: u64, nb_seconds_to_advance: u64) {
+        let mut block_info = self.block_info();
+        block_info.height += nb_blocks_to_advance;
+        block_info.time = block_info.time.plus_seconds(nb_seconds_to_advance);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        self.set_block(block_info);
+    }
+}
+
+pub trait CashMachine {
+    fn mint_native(&mut self, beneficiary: &str, to_mint: Coin);
+}
+
+impl CashMachine for App {
+    fn mint_native(&mut self, beneficiary: &str, to_mint: Coin) {
+        self.sudo(SudoMsg::Bank(BankSudo::Mint {
+            to_address: beneficiary.to_string(),
+            amount: vec![to_mint],
+        }))
+        .unwrap();
     }
 }
